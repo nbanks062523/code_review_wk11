6a9f6fab93c130f238f78819a8f8828cfbb4deb0
nbanks062523 <nbanks@kingsleytechpartners.com>
2024-01-28 07:22:19 -0500

    updated code review file

--- dags/code_review.py
+++ dags/code_review.py
@@ -1,4 +1,6 @@
 from datetime import timedelta, datetime
+import random
+import io 
 
 # Operators#
 from airflow.operators.dummy_operator import DummyOperator
@@ -12,10 +14,17 @@ from airflow.utils.dates import days_ago # scheduling tool that allows us to wri
 apples = ["pink lady", "jazz", "orange pippin", "granny smith", "red delicious", "gala", "honeycrisp", "mcintosh", "fuji"]
 
 # This function will open a text file, read the name from the file and print the name with a hello
-def print_hello():
+file_path = '/opt/airflow/dags/code_review.txt'
+
+def print_hello(file_path):
+    with open(file_path,'r') as f:
+    name = f.read()
+    print(f"hello {name}")
     
-#### This function will randomly select an apple, put it into a string and print the string
-def random_apples():
+# This function will randomly select an apple, put it into a string and print the string
+def random_apples(apples):
+    random_apple= random.choice(apples)
+    print(f"Your apple is: {random_apple}")
 
 ############# Config ########################
 # Create a dictionary of default args for scheduling and retries. 
@@ -35,30 +44,30 @@ default_args = {
 from airflow import DAG
 
 with DAG(
-    'hello', # a unique name for our DAG
-    description='A simple DAG to print "Hello World"', # a description of our DAG
+    'code_review_steps', # a unique name for our DAG
+    description='A DAG that says hello by name and picks random apples"', # a description of our DAG
     default_args=default_args, # pass in the default args.
 ) as dag:
-    description = 'A simple DAG to print "Hello World"', # a description of our DAG
-    default_args = default_args, # pass in the default args defined at line 19
 
     
-    ### create simple tasks: ###
+    ### TASKS ###
+
+# This task will write a name to a text file
+    write_name_task = BashOperator(
+        task_id='write_name', # this is the unique identifier of this task within the DAG
+        bash_command='echo "Nikisha" >  /opt/airflow/dags/code_review.txt' # this is the command that will be run
+    )
+    
+# This task uses a Python operator to call the function 'print_hello()'
+    hello_task = PythonOperator(
+        task_id='say_hello', # this is the unique identifier of this task within the DAG
+        python_callable = print_hello # allows the DAG to execute the function print_hello listed above 
+    )
+
 # The DummyOperator doesn't actually do anything and is used to show graph structure or for grouping tasks in a DAG
     dummy_task = DummyOperator(
         task_id='dummy'
     )
-# The BashOperator allows you to run any bash command
-    hello_task = BashOperator(
-        task_id='print_hello', # this is the unique identifier of this task within the DAG
-        bash_command='echo Hello World' # this is the command that will be run
-    )
-# The PythonOperator 
-    date_task = PythonOperator(
-        task_id="date_task", # this is the unique identifier of this task within the DAG
-        python_callable=print_date # allows the DAG to execute the function print_date listed above 
-    )
-
     # set the task order
     # The example below is using a list
     # the reason for this is to specify that we want 'hello_task' to precede 'date_task' & 'dummy_task'
